20adcd93230be0a2995b776e1ebd3f21598549c0
sewoop <raku@hotmail.co.kr>
2019-08-05 01:54:59 +0900

    v1.7

--- backend/src/Triphub_Room/models.py
+++ backend/src/Triphub_Room/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# 방과 관련된 모델들
+class RoomInput(models.Model):
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    # 날짜
+    roomname = models.CharField(max_length=10)
+    # 방이름
+    mainmember = models.CharField(max_length=10)
+    # 방장
+
+    def __str__(self):
+        return self.roomname
+
+class myRoom(models.Model):
+    room_id = models.CharField(max_length=10)
+    # 방 id
+
+    name = models.CharField(max_length=10)
+    # 멤버들 이름
+
+    def __str__(self):
+        return self.name
+
+# 멤버와 관련된 모델들
+
+class memberList(models.Model):
+    user_id = models.CharField(max_length=20)
+    # 유저 아이디
+    room_id = models.CharField(max_length=10)
+    # 방 id
+
+    def __str__(self):
+        return self.user_id
--- backend/src/Triphub_Room/views.py
+++ backend/src/Triphub_Room/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import RoomInput,myRoom,memberList  # 데이터베이스 틀 임포트
+import pymysql.cursors
+
+# django + react api 연동
+from rest_framework import viewsets
+from .serializers import RoomInputSerializer, myRoomSerializer
+
+@login_required
+def ToRoom_create(request):
+    return render(request, 'create.html')
 
 @login_required
 def Room_create(request):
-    return render(request,'create.html')
+    room = RoomInput()
+    room.roomname = request.GET['roomname'] # 방이름
+    room.mainmember = request.user.username # 방장
+    room.save() # 쿼리셋 메소드 중 하나. room에 넣은 내용을 저장해라는 함수
+    #객체.delete()객체에 해당하는 내용을 데이터베이스 내용을 지워라
+
+    myroom = myRoom()
+    myroom.room_id = room.id
+    myroom.name = room.mainmember
+    myroom.save()
+
+    qs = memberList.objects.all()
+    rows = qs.filter(user_id__icontains=request.user.username)
+    for row in rows:
+        originalvalue = row.room_id
+        originalvalue += ('&' + str(room.id))
+        rows.update(room_id = originalvalue)
+        
+    # memberList.save()
+    # rows.update(room_id += ('&' + room.id))
+
+    return redirect('room/select')
 
 @login_required
-def Room_select(request):
+def Room_select(request, room_id):
     return render(request,'select.html')
 
 @login_required
