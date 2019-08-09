bd1e8c3c8c894ee5f3ddd0624c2d2792db578032
eunju <dmswn334000@naver.com>
2019-08-10 01:02:58 +0900

    tremp

--- backend/src/Triphub_Home/views.py
+++ backend/src/Triphub_Home/views.py
@@ -4,7 +4,7 @@ from Triphub_Room.models import RoomInput, memberList
 
 @login_required
 def main(request):
-
+    
     qs = memberList.objects.all() #유저 정보 모두 가지고 오기
     for row in qs:
         if row.user_id == request.user.username:
@@ -13,18 +13,24 @@ def main(request):
             #유저의 방의 개수 배열 user_room
     
     if len(user_room)!= 0:
-        user_room = user_room[1:]
-    qs = RoomInput.objects.all() #유저 정보 모두 가지고 오기
+        user_room = user_room[1:]#방번호 가지고 오기
 
+    qs = RoomInput.objects.all() #유저 정보 모두 가지고 오기
     info = []
-    for i in user_room:
+    for i in user_room: #방번호로 찾기
         rows = qs.filter(id__icontains=i)
         for row in rows:
             info.append(row)
 
-    return render(request,'main.html', {'infos':info})
+    userName = request.user.username+'님 안녕하세요'
+    return render(request,'main.html', {'infos':info, 'username':userName})
 
-def private_room(request, info_id):
-    room = get_object_or_404(RoomInput, pk = info_id)
-    return render(request,'select.html')
+def private_room(request, info_id): 
+    room_id = get_object_or_404(RoomInput, pk = info_id)
+    room = RoomInput()  #roominput테이블 정보를 대입
+    
+    qs = RoomInput.objects.all()    #방, 방이름, 방장 정보 가져오기
+    row = qs.filter(id__icontains=info_id) #url의 pk값이랑 같은 것을 찾는다
+
+    return render(request,'select.html',{'roomname':row[0].roomname})
 
